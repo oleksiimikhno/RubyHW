b2de9ec40e5f9fe5d8411960a5311f4cbac3d246
Aleksey <xioflesh@gmail.com>
2022-10-19 23:20:33 +0300

    fix to one string

--- HW1/2.rb
+++ HW1/2.rb
@@ -1,14 +1,3 @@
 p "Дан целочисленный массив. Необходимо вывести вначале его элементы с нечетными индексами, а затем - четными."
-
-odd = []
-even = []
-
-[5, 4, 3, 2, 1].each_with_index do |value, index| 
-    if index.odd?
-        odd << value
-    else 
-        even << value
-    end
-end 
-
-p odd + even
+p "p [5, 4, 3, 2, 1, 15, 22, 33].map.with_index{ |value, index| [value, index] }.sort_by{ |value, index| index % -2}.map{ |value, index| value }"
+p [5, 4, 3, 2, 1, 15, 22, 33].map.with_index{ |value, index| [value, index] }.sort_by{ |value, index| index % -2}.map{ |value, index| value }
