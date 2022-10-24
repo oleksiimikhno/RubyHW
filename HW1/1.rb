510ca0368fa9d221b972191fd85ac1138733dd28
Oleksii Mikhno <xioflesh@gmail.com>
2022-10-24 22:11:52 +0300

    test

--- HW1/1.rb
+++ HW1/1.rb
@@ -1,3 +1,3 @@
 p "Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными."
-p "[5, 4, 3, 2, 1].map.with_index{ |value, index| [value, index] }.sort_by{ |value, index| index % 2 }.map{ |value, index| value }"
-p [5, 4, 3, 2, 1].map.with_index{ |value, index| [value, index] }.sort_by{ |value, index| index % 2 }.map{ |value, index| value }
+p "[5, 4, 3, 2, 1].map.with_index{|value, index| [value, index] }.sort_by{ |value, index| index % 2 }.map{ |value, index| value }"
+p [5, 4, 3, 2, 1].map.with_index{|value, index| [value, index] }.sort_by{ |value, index| index % 2 }.map{ |value, index| value }
