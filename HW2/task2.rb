897ce208b787c841a907ce3c0f6453ae3254906d
Oleksii Mikhno <xioflesh@gmail.com>
2022-10-27 23:18:03 +0300

    add hendler user action

--- HW2/task2.rb
+++ HW2/task2.rb
@@ -1,16 +1,36 @@
-# app/models/user.rb
 
 # Create new creation in Tamagotchi game
 class Pet
   def initialize(name = 'Unknown creation')
+    @start = false
+
     @name = name
     @health = health(rand(1..6))
     puts "#{@name} burn."
+
+
+    
+
+    start_game
+  end
+
+  def start_game
+    @start = true
+    @custom_methods = self.class.instance_methods(false)
+
+    @game_methods = @start ? @custom_methods.join(' ').split(' ').reject { |v| v == "start_game" } : @custom_methods
+
+    action_message
   end
 
   def walk
     puts 'You and your pets walk outside'
-    time_past
+    time_pass
+  end
+
+  def sleep
+    puts "#{@name} sleep now..."
+    time_pass
   end
 
   def info
@@ -22,9 +42,18 @@ class Pet
     "
   end
 
+  def minus
+    health(@health.size - 1)
+  end
+
+  def add
+    health(@health.size + 1)
+  end
+
   private
 
-  def time_past
+  # Pet methods
+  def time_pass
     # puts age(@age += 1)
     # health(@health - 1)
     # puts @health
@@ -36,6 +65,7 @@ class Pet
   end
 
   def health(value)
+    p value
     @health = value.times.map { '♥' }.join(' ')
   end
 
@@ -46,9 +76,37 @@ class Pet
   def dead?
     @health.split(' ').to_a.size <= 0
   end
+
+  # User action
+  def action_message
+    title = "\n--------- Please select method name to interact with #{@name}! ---------"
+    subtitle = '________________________________________________________________________________'
+
+    puts "#{title}\n#{@game_methods.join("\n")}\n#{subtitle}
+    "
+
+    user_action(gets.chomp)
+  end
+
+  def user_action(action)
+    !action.empty? ? action_reducer(action) : (puts '▼ Empty action, please type anything from there ▼▼▼! ▼')
+
+    action_message
+  end
+  
+  def action_reducer(action)
+    case true
+    when action_helper(action)
+      eval("self.#{action}")
+    else
+      puts "▼ Unknown command #{action.upcase}, please select current! ▼"
+    end
+    action_message
+  end
+
+  def action_helper(action)
+    @game_methods.join(' ').split(' ').include? action.downcase
+  end
 end
 
 pet = Pet.new
-
-pet.info
-pet.info
