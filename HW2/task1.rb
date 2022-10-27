08dc4931bab775f72a7bb10198f7f0376d126c64
Oleksii Mikhno <xioflesh@gmail.com>
2022-10-27 17:54:29 +0300

    PET add new methods

--- HW2/task1.rb
+++ HW2/task1.rb
@@ -1,4 +1,5 @@
-arr = [621, 445, 147, 159, 430, 222, 482, 44, 194, 522, 652, 494, 14, 126, 532, 387, 441, 471, 337, 446, 18, 36, 202, 574, 556, 458, 16, 139, 222, 220, 107, 82, 264, 366, 501, 319, 314, 430, 55, 336]
+arr = [621, 445, 147, 159, 430, 222, 482, 44, 194, 522, 652, 494, 14, 126, 532, 387, 441, 471, 337, 446, 18, 36, 202,
+       574, 556, 458, 16, 139, 222, 220, 107, 82, 264, 366, 501, 319, 314, 430, 55, 336]
 
 p arr.size
 p arr.reverse
@@ -11,7 +12,7 @@ p arr.select { |v| (v % 2).zero? ? v : nil }
 p arr.select { |v| (v % 3).zero? ? v : nil }
 p arr.uniq
 p arr.map { |v| v.to_f / 10 }
-p arr.map.with_index { |v, i| arr[i] <= ('a'..'z').to_a.size  ? ('a'..'z').to_a[i - 1] : nil }.compact
+p arr.map.with_index { |_v, i| arr[i] <= ('a'..'z').to_a.size ? ('a'..'z').to_a[i - 1] : nil }.compact
 p arr.map { |v| v == arr.max || v == arr.min ? [v == arr.min ? arr.max : arr.min] : v }.flatten
-p arr.take_while { |v| v > arr.min}
+p arr.take_while { |v| v > arr.min }
 p arr.sort.take(3)
--- HW2/task2.rb
+++ HW2/task2.rb
@@ -1,9 +1,54 @@
-class Per {
-    def initilaizer (name = 'Unknown creator') {
+# app/models/user.rb
 
-    }
-}
+# Create new creation in Tamagotchi game
+class Pet
+  def initialize(name = 'Unknown creation')
+    @name = name
+    @health = health(rand(1..6))
+    puts "#{@name} burn."
+  end
 
-per1 = Per.new('Orange')
+  def walk
+    puts 'You and your pets walk outside'
+    time_past
+  end
 
-123
+  def info
+    puts "
+    ________#{@name}________
+    HP: #{@health}
+    age creation: #{@age}
+    ☻☹㋡
+    "
+  end
+
+  private
+
+  def time_past
+    # puts age(@age += 1)
+    # health(@health - 1)
+    # puts @health
+
+    if dead?
+      puts 'Your creation is dead'
+    end
+    # puts info
+  end
+
+  def health(value)
+    @health = value.times.map { '♥' }.join(' ')
+  end
+
+  def age(value = 0)
+    @age = value
+  end
+
+  def dead?
+    @health.split(' ').to_a.size <= 0
+  end
+end
+
+pet = Pet.new
+
+pet.info
+pet.info
