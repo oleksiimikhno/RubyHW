137bd33920d24f5298ce23d9e32526a90f9395a7
Oleksii Mikhno <xioflesh@gmail.com>
2022-11-03 21:55:48 +0200

    final reg inner html

--- HW3/lib/inner_html_content.rb
+++ HW3/lib/inner_html_content.rb
@@ -1,41 +1,35 @@
-require 'erb'
+# frozen_string_literal: true
 
 # Gem append content in html body
 module InnerHTMLContent
-  def self.add_content(content, bypass_html: true, file_name: 'index.html')
-    content = bypass_html ? content.gsub!(%r{<\w+>|</\w+>}, '') : content
+  class << self
+    def add_content(content, bypass_html: true, file_name: 'index.html')
+      serialize_content = content.gsub(%r{<\w+>|</\w+>}, '')
+      content = bypass_html && serialize_content ? content.gsub(%r{<\w+>|</\w+>}, '') : content
 
-    puts template_html(file_name)
-    puts File.read(file_name)
-
-    file_data = File.read(file_name).partition('<body>')
-    new_file_data = file_data.insert(2, "\n#{content}").join
-    File.write(file_name, new_file_data)
-  end
-
-  private
-
-  def template_html(file_name = 'index.html')
-    file = File.new(file_name, 'w')
-
-
-    File.open(file_name, "w") do |file|     
-      file.write(default_template)   
+      template_html(file_name, content)
     end
-    default_template = '
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Title page</title>
-      </head>
-      <body>
-        {html_content}
-      </body>
-    </html>'
-    file.write(file_name, default_template)
-    rr = File.read(file_name)
+
+    private
+
+    def template_html(file_name, content)
+      template = '<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Title page</title>
+        </head>
+        <body>
+        inner_content
+        </body>
+      </html>'
+      template = template.gsub('inner_content', content)
+
+      File.open(file_name, 'w') do |file|
+        file.write(template)
+      end
+    end
   end
 end
