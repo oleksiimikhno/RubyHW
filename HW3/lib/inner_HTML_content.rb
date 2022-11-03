176588bd24a15f1811eaffc25be16c2cdae7b15e
Oleksii Mikhno <xioflesh@gmail.com>
2022-11-03 17:56:31 +0200

    rework inner_HTML

--- HW3/lib/inner_HTML_content.rb
+++
@@ -1,8 +0,0 @@
-# Gem append content in html body
-module InnerHTMLContent
-  def self.add_content(content, file_name = 'index.html')
-    file_data = File.read(file_name).partition('<body>')
-    new_file_data = file_data.insert(2, "\n#{content}").join
-    File.write(file_name, new_file_data)
-  end
-end
