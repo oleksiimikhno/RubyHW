d541778d32342f97c65e9ec8a6fe7fd8fc85a91f
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-09 17:21:50 +0200

    clear filte

--- HW8/blog-RoR-api/app/models/article.rb
+++ HW8/blog-RoR-api/app/models/article.rb
@@ -4,9 +4,6 @@ class Article < ApplicationRecord
   has_and_belongs_to_many :tags
   has_many :likes, as: :likeable
 
-  # has_many :filter_by_status, -> { filter_by_status }, class_name: 'Comment'
-  # has_many :filter_by_last_items_limit, -> { filter_by_last_items_limit }, class_name: 'Comment'
-
   enum :status, [ :published, :unpublished ], default: :unpublished
 
   validates :title, presence: true, length: { in: 2..20 }
