f840201f4a244de4e42b2c9b88912222cd4dd39a
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-11 20:00:23 +0200

    update route

--- HW5-6-7/blog-RoR-api/config/routes.rb
+++ HW5-6-7/blog-RoR-api/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
           get :comments
           get :published
           get :unpublished
-          post 'add-tags', to: 'articles#add_tags'
+          post 'add-tag', to: 'articles#add_tag'
         end
       end
 
