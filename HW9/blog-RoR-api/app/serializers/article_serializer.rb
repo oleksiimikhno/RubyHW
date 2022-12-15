d4be9e6b52282f00f32fbfdae8026c5a519ecdee
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-15 22:07:32 +0200

    add in serializer status

--- HW9/blog-RoR-api/app/serializers/article_serializer.rb
+++ HW9/blog-RoR-api/app/serializers/article_serializer.rb
@@ -1,5 +1,5 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :created_at
+  attributes :id, :title, :body, :status, :created_at
 
   has_one :author, serializer: AuthorSerializer
   has_many :comments, each_serializer: CommentSerializer
