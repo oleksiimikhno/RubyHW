992dc9b163d148843053d2f8c7b96a51b838919e
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-17 23:39:53 +0200

    remove route switch

--- HW9/blog-RoR-api/config/routes.rb
+++ HW9/blog-RoR-api/config/routes.rb
@@ -18,7 +18,6 @@ Rails.application.routes.draw do
           get :published
           get :unpublished
         end
-        patch :switch, to: 'comments#switch_status', on: :member
       end
 
       resources :tags
