06073d861efc1b75daa2ae63d6bdf6f3551c91d9
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-22 16:21:55 +0200

    update controller products

--- HW10/store/app/controllers/products_controller.rb
+++ HW10/store/app/controllers/products_controller.rb
@@ -2,6 +2,34 @@ class ProductsController < ApplicationController
   # before_action :set_product, only: %i[show update destroy]
   # before_action :set_products, only: %i[index]
 
+  def index
+    @products
+
+    render :products
+  end
+
+  def show
+    @product
+  end
+
+  def create
+    @product = Article.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product.destroy
+  end
+
   private
 
   def set_product
