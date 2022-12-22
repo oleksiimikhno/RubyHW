42da08d894b2fbc8908702ce3a8d47c8c6fff73c
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-22 20:39:39 +0200

    move seeds

--- HW10/store2/db/seeds.rb
+++ HW10/store2/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = Category.create([
+  { name: 'Phones',
+    description: 'Category description' }
+])
+
+products = Product.create([
+  { name: 'Iphone 14',
+    description: 'iPhone 14 Pro has Dynamic Island, a magical new way to interact with iPhone.
+                  And an Always-On display, which keeps your important info at a glance.',
+    image: '/images/apple/iphone/14',
+    price: 999.99, category_id: 1 }
+])
