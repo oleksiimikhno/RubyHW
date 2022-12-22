0a84676178442ec1ed259f0725004eac52fe62a0
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-22 20:13:50 +0200

    add store2

---
+++ HW10/store2/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_for :products
+  # devise_for :admin_users, ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
+
+  # devise_for :users
+  # resources :users
+
+  resources :products
+  resources :categories
+
+  root 'products#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
