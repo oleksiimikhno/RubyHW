513e06350895b362be23f458aa1354bbdf8294b6
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-23 21:09:00 +0200

    add log out button

--- HW10/store/config/routes.rb
+++ HW10/store/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :products
   resources :categories
