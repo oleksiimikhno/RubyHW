757070d814da0077c73f1f6c43fa3a23d8306475
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-02 22:52:00 +0200

    add current cart

--- HW11/store/app/controllers/application_controller.rb
+++ HW11/store/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  before_action :current_cart
+
+  private
+
+  def current_cart
+    @current_cart = Cart.create
+  end
 end
