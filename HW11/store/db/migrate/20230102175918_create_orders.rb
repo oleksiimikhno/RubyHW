94aa761d298cab3a13b39687f7e0065273e894ba
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-02 20:01:52 +0200

    add  models cart line_item and order

---
+++ HW11/store/db/migrate/20230102175918_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.enum :status
+      t.string :name
+      t.string :email
+      t.text :address
+      t.text :payment
+
+      t.timestamps
+    end
+  end
+end
