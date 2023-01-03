4df8a0fcc39668999d12ce031ce321c1fb911200
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-03 10:33:45 +0200

    add hendler cookies cart

--- HW11/store/app/controllers/application_controller.rb
+++ HW11/store/app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
-  # before_action :current_cart
+  before_action :handle_cookies
   helper_method :current_cart
 
   private
 
+  def handle_cookies
+    cookies[:cart_id] = Cart.create.id unless cookies[:cart_id].present?
+  end
 
   def current_cart
-    debugger
-    Cart.find_or_create_by(cookies[:cart_id])
+    Cart.find(cookies[:cart_id])
   end
-  # def current_cart
-  #   Cart.find_or_create_by
-  # end
 end
