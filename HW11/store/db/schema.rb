1fe7712f3afa96ac8fb0dacb963274a9155370a0
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-03 22:58:26 +0200

    add migration

--- HW11/store/db/schema.rb
+++ HW11/store/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_175918) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_03_202837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_175918) do
     t.text "payment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "cart_id", null: false
+    t.index ["cart_id"], name: "index_orders_on_cart_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -94,5 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_175918) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "carts"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
 end
