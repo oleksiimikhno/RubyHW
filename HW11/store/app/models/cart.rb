68ce3901bf9c3c184e03fb54887b4f2b1c187dae
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-04 02:51:57 +0200

    rework total_price

--- HW11/store/app/models/cart.rb
+++ HW11/store/app/models/cart.rb
@@ -3,6 +3,6 @@ class Cart < ApplicationRecord
   has_one :order, dependent: :nullify
 
   def total_price
-    self.line_items.map(&:total_price).sum
+    line_items.map(&:total_price).sum
   end
 end
--- HW11/store/app/models/line_item.rb
+++ HW11/store/app/models/line_item.rb
@@ -6,6 +6,6 @@ class LineItem < ApplicationRecord
   validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 
   def total_price
-    self.quantity * self.product.price
+    quantity * product.price
   end
 end
