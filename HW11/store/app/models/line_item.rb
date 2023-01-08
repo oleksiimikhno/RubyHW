5d0c6312bf084cdc7a5433e599c6beaf384b7993
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-08 23:23:37 +0200

    add gem annotated

--- HW11/store/app/models/line_item.rb
+++ HW11/store/app/models/line_item.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: line_items
+#
+#  id         :bigint           not null, primary key
+#  quantity   :integer          default(1)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  cart_id    :integer
+#  order_id   :integer
+#  product_id :integer
+#
 class LineItem < ApplicationRecord
   belongs_to :cart
   # belongs_to :order
