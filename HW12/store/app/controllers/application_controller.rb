b5ef2fda545a0e8c3f3fd504cd0d4809556303e6
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-12 22:54:31 +0200

    add helper categories

--- HW12/store/app/controllers/application_controller.rb
+++ HW12/store/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :handle_cookies
-  helper_method :current_cart, :cart_total_quantity
+  helper_method :current_cart, :cart_total_quantity, :categories
 
   private
 
@@ -14,5 +14,9 @@ class ApplicationController < ActionController::Base
 
   def cart_total_quantity
     current_cart.line_items.includes(:product).sum(&:quantity)
+  end
+
+  def categories
+    Category.order(:title)
   end
 end
