19a867c74ac06858c55f09e9e5d61d21af6c6b4b
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-12 19:34:05 +0200

    clear code

--- HW11/store/app/controllers/categories_controller.rb
+++ HW11/store/app/controllers/categories_controller.rb
@@ -1,45 +1,8 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[show edit update destroy]
-
-  def index
-    @categories = Category.all
-  end
-
   def show
+    @category = Category.find(params[:id])
     @products = @category.products
 
-    unless @products.exist?
-      flash.now[:alert] = 'Products in category not found'
-    end
-  end
-
-  def create
-    @category = Category.new(category_params)
-
-    if @category.save
-      redirect_to @category, notice: 'Category was successfully created.'
-    else
-      render :show, status: :unprocessable_entity
-    end
-  end
-
-  def edit; end
-
-  def destroy
-    @category.destroy
-  end
-
-  private
-
-  def set_category
-    @category = Category.find_by(id: params[:id])
-    if @category.nil?
-      flash.now[:alert] = 'Your category was not found'
-      render template: 'layouts/404'
-    end
-  end
-
-  def category_params
-    params.require(:category).permit(:name, :description, :price, :image)
+    flash.now[:alert] = 'Products in category not found' unless @products.exists?
   end
 end
