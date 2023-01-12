3cd08bb5311e4d170c19dfadc9e0acfb0f8ae351
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-12 17:55:26 +0200

    remove response to form

--- HW12/store/app/controllers/carts_controller.rb
+++ HW12/store/app/controllers/carts_controller.rb
@@ -4,10 +4,5 @@ class CartsController < ApplicationController
   def show
     @products = current_cart.line_items.map { |item| { product: item.product, quantity: item.quantity } }
     @line_items = current_cart.line_items
-
-    respond_to do |format|
-      format.html
-      format.json { render json: { products: @products } }
-    end
   end
 end
