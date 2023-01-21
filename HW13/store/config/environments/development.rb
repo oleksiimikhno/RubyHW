47684ab32baaaf925ca1be31d2af16c92a415296
Oleksii Mikhno <xioflesh@gmail.com>
2023-01-21 22:47:14 +0200

    enable imagemagick

--- HW13/store/config/environments/development.rb
+++ HW13/store/config/environments/development.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Enable magick gem
+  config.active_storage.variant_processor = :mini_magick
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
